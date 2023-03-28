f0de9ddb14502556ce8d7bbf0fd8d2fe61b2860f
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-28 07:54:49 -0700

    mainn.h

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
@@ -19,8 +20,9 @@
 
 /**
  * struct fmt - Struct op
+ *
  * @fmt: The format.
- * @fn: The function associated
+ * @fn: The function associated.
  */
 struct fmt
 {
@@ -28,13 +30,14 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
+
 /**
  * typedef struct fmt fmt_t - Struct op
+ *
  * @fmt: The format.
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
